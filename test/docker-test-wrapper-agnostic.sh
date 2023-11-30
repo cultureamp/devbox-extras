934083c1355efb431fdefc08d8ba983adab934d3
t-monaghan <tom.monaghan@cultureamp.com>
2023-12-01 10:10:44 +1100

    removes sed cmd, keeps sudo in bootstrap

--- test/docker-test-wrapper-agnostic.sh
+++ test/docker-test-wrapper-agnostic.sh
@@ -17,10 +17,4 @@ echo '
 echo "security ran with args: $@" > /tmp/test-metadata/security.txt' | sudo tee -a /bin/security
 sudo chmod a+x /bin/security
 
-# The below removes all sudo commands from install script
-# Required as the docker container runs as root and the docker container 
-# must be run as root because the nix installer only supports single user 
-# root installs in containers 
-# sed -i 's/sudo / /g' ./scripts/bootstrap.sh 
-
 . ./scripts/bootstrap.sh
