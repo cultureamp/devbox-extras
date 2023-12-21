4336944209f82560dc9238709c7a751267b6bcc4
Jack Rose <user@jackrose.co.nz>
2023-12-21 19:25:49 +1100

    swap out nix lint/fix for devbox style

--- flake.nix
+++ flake.nix
@@ -10,45 +10,10 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { system = "${system}"; config.allowUnfree = true; };
-
-        # common parts of making derivations for flake checks
-        # from: https://msfjarvis.dev/posts/writing-your-own-nix-flake-checks/
-        mkCheck = { name, nativeBuildInputs, checkPhase }: pkgs.stdenvNoCC.mkDerivation {
-          inherit name nativeBuildInputs checkPhase;
-          src = ./.;
-          dontBuild = true;
-          doCheck = true;
-          installPhase = ''
-            mkdir "$out"
-          '';
-        };
       in
       {
         # set formatter binary for `nix fmt` command
         formatter = pkgs.nixpkgs-fmt;
-
-        checks = {
-          nix-formatting = mkCheck {
-            name = "nix-formatting";
-            nativeBuildInputs = [ pkgs.nixpkgs-fmt ];
-            checkPhase = "nixpkgs-fmt --check .";
-          };
-          nix-linting = mkCheck {
-            name = "nix-linting";
-            nativeBuildInputs = [ pkgs.statix ];
-            checkPhase = "statix check";
-          };
-          shell-linting = mkCheck {
-            name = "shell-linting";
-            nativeBuildInputs = [ pkgs.shellcheck ];
-            checkPhase = "shellcheck **/*.{sh,bash,bats}";
-          };
-          shell-formatting = mkCheck {
-            name = "shell-formatting";
-            nativeBuildInputs = [ pkgs.shfmt ];
-            checkPhase = "shfmt --write **/*.{sh,bash,bats}";
-          };
-        };
 
         packages = {
           mongodb-4_4 = pkgs.callPackage ./packages/mongodb-4_4.nix { };
