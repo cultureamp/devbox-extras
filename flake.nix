baa650dd66f224bac7bc48bc536c7040946df993
Jack Rose <user@jackrose.co.nz>
2023-12-21 18:35:41 +1100

    add nix flake check

--- flake.nix
+++ flake.nix
@@ -7,14 +7,30 @@
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
-    # this flake only works for darwin binaries (for now)
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { system = "${system}"; config.allowUnfree = true; };
+
+        # common parts of making derivations for flake checks
+        # from: https://msfjarvis.dev/posts/writing-your-own-nix-flake-checks/
+        mkCheck = nativeBuildInputs: checkPhase: pkgs.stdenvNoCC.mkDerivation {
+          inherit nativeBuildInputs checkPhase;
+          name = "flake-check";
+          src = ./.;
+          dontBuild = true;
+          doCheck = true;
+          installPhase = '' mkdir "$out" '';
+        };
       in
       {
         # set formatter binary for `nix fmt` command
         formatter = pkgs.nixpkgs-fmt;
+
+        checks = {
+          nix-formatting = mkCheck [ pkgs.nixpkgs-fmt ] "nixpkgs-fmt --check .";
+          nix-linting = mkCheck [ pkgs.statix ] "statix check";
+          shellcheck = mkCheck [ pkgs.shellcheck ] "shellcheck **/*.sh";
+        };
 
         packages = {
           mongodb-4_4 = pkgs.callPackage ./packages/mongodb-4_4.nix { };
