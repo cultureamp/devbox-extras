7ac563f3f8a7870902e612e69f8831c6f94cf825
Will Brennan <will.brennan@cultureamp.com>
2024-03-12 16:05:09 +1100

    Add the start of the types

--- plugins/intellij/src/jdk-table-types.ts
+++ plugins/intellij/src/jdk-table-types.ts
@@ -1,12 +1,73 @@
-export default type = [
+type Application = {
+  application: Component[]
+}
+
+type KeyValues = {
+  version?: string
+  value?: string
+  type?: string
+  url?: string
+  name?: string
+}
+
+type Component = {
+  component: Jdk[]
+  ":@": Pick<KeyValues, "name">
+}
+
+type Jdk = {
+  jdk: JdkChildren[]
+  ":@": Pick<KeyValues, "version">
+}
+
+type JdkChildren = Name | Type | Version | HomePath | Roots | Additional
+
+type Name = {
+  name: []
+  ":@": Pick<KeyValues, "value">
+}
+
+type Type = {
+  type: []
+  ":@": Pick<KeyValues, "value">
+}
+
+type Version = {
+  version: []
+  ":@": Pick<KeyValues, "value">
+}
+
+type HomePath = {
+  homePath: []
+  ":@": Pick<KeyValues, "value">
+}
+
+type Roots = {
+  roots: []
+}
+
+type Additional = {
+  additional: []
+}
+
+export type JdkTable = Application[]
+
+const jdkTable: JdkTable = [
   {
-    application : [
-      {component : [
-        
-      ], ':@' : {
-          version : string
-        }
-      }
-    ]
-  }
+    application: [
+      {
+        component: [
+          {
+            jdk: [],
+            ":@": {
+              version: "2",
+            },
+          },
+        ],
+        ":@": {
+          name: "11",
+        },
+      },
+    ],
+  },
 ]
