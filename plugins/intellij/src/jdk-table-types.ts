32485017e26541eddf3fcb8f92c344a0e1f760ea
t-monaghan <tom.monaghan@cultureamp.com>
2024-03-13 10:20:54 +1100

    WIP pairing

--- plugins/intellij/src/jdk-table-types.ts
+++ plugins/intellij/src/jdk-table-types.ts
@@ -1,3 +1,5 @@
+// NOTE: May need to export injected types if inferring not working
+
 type KeyValues = "version" | "value" | "type" | "url" | "name"
 
 interface ChildArray<QueryKey extends string, Child> {
@@ -28,13 +30,34 @@ type Version = XmlElement<"version", "value", undefined>
 type HomePath = XmlElement<"homePath", "value", undefined>
 type Additional = XmlElement<"additional", undefined, undefined>
 
-type Roots = XmlElement<"roots", undefined, undefined>
-// Roots has missing children
+type ParentRoot = XmlElement<"root", "type", ChildRoot> 
+
+type ChildRoot = {
+  root: []  
+  ":@": {
+    url: string
+    type: string
+  }
+}
+
+type Roots = XmlElement<"roots", undefined, RootsChildren>
+
+type RootsChildren = AnnotationsPath | ClassPath | SourcePath | JavaDocPath
+
+type AnnotationsPath = XmlElement<"annotationsPath", undefined, ParentRoot>
+
+type SourcePath = XmlElement<"sourcePath", undefined, ParentRoot>
+
+type ClassPath = XmlElement<"classPath", undefined, ParentRoot>
+
+type JavaDocPath = XmlElement<"javaDocPath", undefined, ParentRoot>
 
 type JdkChildren = Name | Type | Version | HomePath | Roots | Additional
 
-type Component = XmlElement<"component", "name", JDK>
-type JDK = XmlElement<"jdk", "version", JdkChildren>
+type Component = XmlElement<"component", "name", Jdk>
+
+type Jdk = XmlElement<"jdk", "version", JdkChildren>
+
 type Application = XmlElement<"application", undefined, Component>
 
-// We are missing the types for Everything below the roots element
+export type Applications = Application[]
