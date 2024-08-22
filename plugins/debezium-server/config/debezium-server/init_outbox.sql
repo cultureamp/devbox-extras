7fcb2256455092613d91d623785474a50425c9a4
Em D <13535330+m-dwyer@users.noreply.github.com>
2024-08-22 17:40:21 +1000

    add pnpm package, explicitly add SCHEMA_REGISTRY_URL and pass heartbeat table to init script

--- plugins/debezium-server/config/debezium-server/init_outbox.sql
+++ plugins/debezium-server/config/debezium-server/init_outbox.sql
@@ -1,11 +1,13 @@
 \echo Creating debezium user: :debezium_user
 \echo Using schema: :schema_name
-\echo Creating outbox table: :table_name
+\echo Creating outbox table: :outbox_table_name
+\echo Creating heartbeat table: :heartbeat_table_name
 
 SET vars.debezium_user TO :debezium_user;
 SET vars.debezium_password TO :debezium_password;
 SET vars.schema_name TO :schema_name;
-SET vars.table_name TO :table_name;
+SET vars.outbox_table_name TO :outbox_table_name;
+SET vars.heartbeat_table_name TO :heartbeat_table_name;
 
 -- Basic permissions
 DO $$
@@ -35,10 +37,10 @@ END $$;
 DO $$
 DECLARE
     schema_name text := current_setting('vars.schema_name');
-    table_name text := current_setting('vars.table_name');
+    outbox_table_name text := current_setting('vars.outbox_table_name');
 BEGIN
     -- Create table if it does not exist
-    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = table_name AND schemaname = schema_name) THEN
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = outbox_table_name AND schemaname = schema_name) THEN
         EXECUTE FORMAT('
         CREATE TABLE %s.%s (
             id UUID NOT NULL PRIMARY KEY,
@@ -48,12 +50,12 @@ BEGIN
             payload BYTEA NOT NULL,
             account_id UUID NOT NULL,
             created_at TIMESTAMP NOT NULL
-        )', schema_name, table_name);
+        )', schema_name, outbox_table_name);
     END IF;
 
     -- Set table owner if not already set
-    IF (SELECT tableowner FROM pg_tables WHERE tablename = table_name AND schemaname = schema_name) != 'dbz_replication_role' THEN
-        EXECUTE FORMAT('ALTER TABLE %s.%s OWNER TO dbz_replication_role', schema_name, table_name);
+    IF (SELECT tableowner FROM pg_tables WHERE tablename = outbox_table_name AND schemaname = schema_name) != 'dbz_replication_role' THEN
+        EXECUTE FORMAT('ALTER TABLE %s.%s OWNER TO dbz_replication_role', schema_name, outbox_table_name);
     END IF;
 END $$;
 
@@ -61,6 +63,7 @@ END $$;
 DO $$
 DECLARE
     schema_name text := current_setting('vars.schema_name');
+    heartbeat_table_name text := current_setting('vars.heartbeat_table_name');
 BEGIN
     -- Create the sequence if it doesn't exist
     EXECUTE FORMAT('
@@ -73,25 +76,26 @@ BEGIN
 
     -- Create table if it does not exist
     EXECUTE FORMAT('
-    CREATE TABLE IF NOT EXISTS %s.debezium_heartbeat (
+    CREATE TABLE IF NOT EXISTS %s.%s (
     id bigint NOT NULL DEFAULT nextval(''%s.debezium_heartbeat_id_seq''::regclass),
     ts timestamp without time zone NOT NULL,
     CONSTRAINT debezium_heartbeat_id_key UNIQUE (id)
-    );', schema_name, schema_name);
+    );', schema_name, heartbeat_table_name, schema_name);
 
-    EXECUTE FORMAT('ALTER TABLE %s.debezium_heartbeat OWNER TO dbz_replication_role;', schema_name);
+    EXECUTE FORMAT('ALTER TABLE %s.%s OWNER TO dbz_replication_role;', schema_name, heartbeat_table_name);
 END $$;
 
 -- Create publication
 DO $$
 DECLARE
     schema_name text := current_setting('vars.schema_name');
-    table_name text := current_setting('vars.table_name');
+    outbox_table_name text := current_setting('vars.outbox_table_name');
+    heartbeat_table_name text := current_setting('vars.heartbeat_table_name');
 BEGIN
     -- Create publication if it does not exist
-    IF NOT EXISTS (SELECT 1 FROM pg_publication_tables WHERE pubname = 'dbz_publication' AND schemaname = schema_name AND tablename = table_name) THEN
+    IF NOT EXISTS (SELECT 1 FROM pg_publication_tables WHERE pubname = 'dbz_publication' AND schemaname = schema_name AND tablename = outbox_table_name) THEN
         DROP PUBLICATION IF EXISTS dbz_publication;
-        EXECUTE FORMAT('CREATE PUBLICATION dbz_publication FOR TABLE %s.%s, %s.debezium_heartbeat;', schema_name, table_name, schema_name);
+        EXECUTE FORMAT('CREATE PUBLICATION dbz_publication FOR TABLE %s.%s, %s.%s;', schema_name, outbox_table_name, schema_name, heartbeat_table_name);
     END IF;
 
     -- Set publication owner if not already set
