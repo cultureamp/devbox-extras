8a29658f629fd8b66a82e96b3cba7b28b1092bd5
t-monaghan <tom.monaghan@cultureamp.com>
2024-09-05 15:03:00 +1000

    github auth in ruby

--- scripts/install_hotel.rb
+++ scripts/install_hotel.rb
@@ -1,142 +1,120 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "net/http"
-require "json"
-require "uri"
-require "fileutils"
+require 'net/http'
+require 'json'
+require 'uri'
+require 'fileutils'
 
-CLIENT_ID = ENV["GITHUB_APP_CLIENT_ID"]
-
-def help
-  puts "usage: app_cli <login | whoami | help>"
-end
-
-def main
-  case ARGV[0]
-  when "help"
-    help
-  when "login"
-    login
-  when "whoami"
-    whoami
-  else
-    puts "Unknown command #{ARGV[0]}"
-  end
-end
+CLIENT_ID = ENV['GITHUB_APP_CLIENT_ID']
 
 def parse_response(response)
   case response
   when Net::HTTPOK, Net::HTTPCreated
     JSON.parse(response.body)
   when Net::HTTPUnauthorized
-    puts "You are not authorized. Run the `login` command."
+    puts 'You are not authorized. Run the `login` command.'
     exit 1
   else
-    puts response
-    puts response.body
+    puts "An error occurred: #{response.code} #{response.message}"
     exit 1
   end
 end
 
 def request_device_code
-  uri = URI("https://github.com/login/device/code")
-  parameters = URI.encode_www_form("client_id" => CLIENT_ID)
-  headers = {"Accept" => "application/json"}
+  uri = URI('https://github.com/login/device/code')
+  parameters = URI.encode_www_form('client_id' => CLIENT_ID)
+  headers = { 'Accept' => 'application/json' }
 
   response = Net::HTTP.post(uri, parameters, headers)
   parse_response(response)
 end
 
 def request_token(device_code)
-  uri = URI("https://github.com/login/oauth/access_token")
+  uri = URI('https://github.com/login/oauth/access_token')
   parameters = URI.encode_www_form({
-    "client_id" => CLIENT_ID,
-    "device_code" => device_code,
-    "grant_type" => "urn:ietf:params:oauth:grant-type:device_code"
-  })
-  headers = {"Accept" => "application/json"}
+                                     'client_id' => CLIENT_ID,
+                                     'device_code' => device_code,
+                                     'grant_type' => 'urn:ietf:params:oauth:grant-type:device_code'
+                                   })
+  headers = { 'Accept' => 'application/json' }
   response = Net::HTTP.post(uri, parameters, headers)
   parse_response(response)
 end
+# rubocop:disable Metrics/MethodLength
 
 def poll_for_token(device_code, interval)
-
   loop do
     response = request_token(device_code)
-    error, access_token = response.values_at("error", "access_token")
+    error, access_token, refresh_token = response.values_at('error', 'access_token', 'refresh_token')
 
     if error
       case error
-      when "authorization_pending"
+      when 'authorization_pending'
         # The user has not yet entered the code.
         # Wait, then poll again.
         sleep interval
         next
-      when "slow_down"
+      when 'slow_down'
         # The app polled too fast.
         # Wait for the interval plus 5 seconds, then poll again.
         sleep interval + 5
         next
-      when "expired_token"
+      when 'expired_token'
         # The `device_code` expired, and the process needs to restart.
-        puts "The device code has expired. Please run `login` again."
+        puts 'The device code has expired. Please run `login` again.'
         exit 1
-      when "access_denied"
+      when 'access_denied'
         # The user cancelled the process. Stop polling.
-        puts "Login cancelled by user."
+        puts 'Login cancelled by user.'
         exit 1
       else
         puts response
         exit 1
       end
-    end
-
-    osName = `uname -s`
-    if osName == "Darwin\n"
-      `security add-generic-password -a "github" -s "com.tmonaghan.hello-world" -w "#{access_token}" -U`
     else
-      # hotel_secrets_path = "#{ENV['XDG_DATA_DIR'] || "#{Dir.home}/.local/share"}/hotel/secrets"
-      # Dir.mkdir(hotel_secrets_path)
-      # File.write(hotel_secrets_path + "/" + "github", access_token)
-      # FileUtils.chmod(0600, "./.token")
+      set_tokens(access_token, refresh_token)
     end
-
     break
   end
 end
+# rubocop:enable Metrics/MethodLength
 
-def login
-  verification_uri, user_code, device_code, interval = request_device_code.values_at("verification_uri", "user_code", "device_code", "interval")
+APP_SERVICE_NAME = 'com.cultureamp.hotel'
+APP_ACCOUNT_NAME = 'github.app'
 
-  # puts "Please visit: #{verification_uri}"
-  `open #{verification_uri}`
-  puts "A browser window should appear, please authorise the : #{user_code}"
+def set_tokens(access_token, refresh_token)
+  add_keychain_item(APP_SERVICE_NAME, APP_ACCOUNT_NAME, access_token)
+  add_keychain_item("#{APP_SERVICE_NAME}.refresh", APP_ACCOUNT_NAME, refresh_token)
+end
+
+def add_keychain_item(service_name, account_name, password)
+  `security add-generic-password -a #{account_name} -s #{service_name} -w #{password} -U`
+rescue StandardError => e
+  puts "An error occurred while setting a token in the keychain: #{e.message}"
+  exit 1
+end
+
+# rubocop:disable Metrics/MethodLength
+def main
+  os_name = `uname -s`
+  if os_name != "Darwin\n"
+    puts 'currently this script only supports MacOS'
+    exit 1
+  end
+  if CLIENT_ID.nil?
+    puts 'Please set the GITHUB_APP_CLIENT_ID environment variable.'
+    exit 1
+  end
+  verification_uri, user_code, device_code, interval = request_device_code.values_at('verification_uri', 'user_code',
+                                                                                     'device_code', 'interval')
+
+  puts "Please visit: #{verification_uri} and enter the following code: #{user_code}"
 
   poll_for_token(device_code, interval)
 
-  puts "Successfully authenticated!"
+  puts 'Successfully authenticated!'
 end
-
-def whoami
-  uri = URI("https://api.github.com/user")
-
-  begin
-    # token = File.read("./.token").strip
-    token = `security find-generic-password -a "github" -s "com.tmonaghan.hello-world" -w`.strip
-  rescue Errno::ENOENT => e
-    puts "You are not authorized. Run the `login` command."
-    exit 1
-  end
-
-  response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-    body = {"access_token" => token}.to_json
-    headers = {"Accept" => "application/vnd.github+json", "Authorization" => "Bearer #{token}"}
-
-    http.send_request("GET", uri.path, body, headers)
-  end
-
-  parsed_response = parse_response(response)
-  puts "You are #{parsed_response["login"]}"
-end
+# rubocop:enable Metrics/MethodLength
 
 main
