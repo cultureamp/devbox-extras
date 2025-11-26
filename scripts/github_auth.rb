3b1e30845fe12d2ba8d53582a1e7473e5c4b36c2
Jack Rose <user@jackrose.co.nz>
2025-11-26 14:34:25 +1100

    set nix config to use token for requests to github.com

--- scripts/github_auth.rb
+++ scripts/github_auth.rb
@@ -106,7 +106,8 @@ def main
 
   set_tokens(access_token, refresh_token)
 
-  `sh -c "$(curl -fsSL https://raw.githubusercontent.com/cultureamp/devbox-extras/main/scripts/install_hotel.sh)"`
+  `export NIX_CONFIG="access-tokens = github.com=#{access_token}" &&
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/cultureamp/devbox-extras/main/scripts/install_hotel.sh)"`
 end
 # rubocop:enable Metrics/MethodLength
 
