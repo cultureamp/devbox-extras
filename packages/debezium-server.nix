d5b270413d19886dba4df4a4438769bbea5aabe7
Shay Qian <shay.qian2@cultureamp.com>
2026-07-27 12:58:12 +1000

    updated debezium package url

--- packages/debezium-server.nix
+++ packages/debezium-server.nix
@@ -15,7 +15,15 @@ stdenv.mkDerivation {
   inherit pname version;
 
   src = fetchzip {
-    url = "https://repo1.maven.org/maven2/io/debezium/debezium-server-dist/${version}/${tarballName}";
+    # Google's Maven Central mirror first, falling back to Central itself.
+    # See: https://cloudplatform.googleblog.com/2015/11/faster-builds-for-Java-developers-with-Maven-Central-mirror.html
+    #
+    # fetchurl tries these in order and stops at the first success, so the second
+    # URL is only used if the mirror fails (e.g. 404 or connection error).
+    urls = [
+      "https://maven-central-asia.storage-download.googleapis.com/maven2/io/debezium/debezium-server-dist/${version}/${tarballName}"
+      "https://repo1.maven.org/maven2/io/debezium/debezium-server-dist/${version}/${tarballName}"
+    ];
     hash = "sha256-RiMBvg9925qcBL04XQ6mKfRg/OznvliSYGjo+HOFzJc=";
   };
 
